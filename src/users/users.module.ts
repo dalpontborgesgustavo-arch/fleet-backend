86facd9d0ee78396ea1097f22d3a8ceac90c6a8f
Gustavo Borges <dalpontborgesgustavo@gmail.com>
2026-04-17 13:12:00 -0300

    fix: import auth module in users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [PrismaModule, AuthModule],
   controllers: [UsersController],
-  providers: [PrismaService],
 })
 export class UsersModule {}
